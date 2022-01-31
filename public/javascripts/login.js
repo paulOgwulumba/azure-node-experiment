b3afb6a97f842065f6438b61ad22472bb8449143
Paul Ogwulumba <pauleugeneogwulumba@gmail.com>
2022-01-31 15:52:48 +0000

    Initial commit

---
+++ public/javascripts/login.js
@@ -0,0 +1,31 @@
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const submitButton = document.getElementById('submit-button');
+
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  // console.log(`username: ${username} .. password: ${password}`);
+
+  const options = {
+    method: 'POST',
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify({name: username, password}),
+  };
+
+  fetch('http://cfcfnapppwsh.azurewebsites.net/api/testHTTP', options)
+    .then((response) => response.json())
+    .then((data) => {
+      const {name, password} = JSON.parse(data);
+      alert(name, password);
+    })
+    .catch((err) => {
+      alert(err);
+    })
+    
+});
